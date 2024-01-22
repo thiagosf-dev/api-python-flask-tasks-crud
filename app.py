80d146d098302a6ed8e9e140948951276c068b1c
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2024-01-22 07:57:03 -0300

    ✨ feat: first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# __name__ = __main__
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+@app.route("/about")
+def about():
+    return "About"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
